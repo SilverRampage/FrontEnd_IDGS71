48d80c0087e7a5270cea29800bdf032795f55194
Emiliano Bermudez <al222110792@gmail.com>
2023-11-29 20:43:52 -0600

    Commit Base_1

---
+++ resources/views/home.blade.php
@@ -0,0 +1,29 @@
+@extends('adminlte::page')
+
+
+
+@section('content_header')
+    <h1 class="m-0 text-dark">Dashboard</h1>
+@stop
+
+@section('content')
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <p class="mb-0">Bienvenido!</p>
+                </div>
+
+                <div class='card body'>
+
+                    
+
+
+                </div>
+
+
+                
+            </div>
+        </div>
+    </div>
+@stop
